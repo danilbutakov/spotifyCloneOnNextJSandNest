ce589a4a91607d3598ebb52ad4a3c2089e7f7e12
danilbutakov <12021109359@irgups.ru>
2023-01-24 00:56:52 +0800

    Сделал типизацию PlayerReducer без ReduxToolKit

---
+++ client/store/index.ts
@@ -0,0 +1,11 @@
+// create a makeStore function
+import { Context, createWrapper } from 'next-redux-wrapper';
+import { createStore, Store } from 'redux';
+import { reducer, RootState } from '@/store/reducers';
+
+const makeStore = (context: Context) => createStore(reducer);
+
+// export an assembled wrapper
+export const wrapper = createWrapper<Store<RootState>>(makeStore, {
+	debug: true
+});
